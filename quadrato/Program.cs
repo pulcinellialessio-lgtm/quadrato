5df6f74cd3cced3b0a1e96bda18fc72a411dbc88
pulcinellialessio-lgtm <pulcinelli.alessio@franchettisalviani.net>
2025-11-18 10:46:00 +0100

    fhg

--- quadrato/Program.cs
+++ quadrato/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        static bool Quadrato(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if( i > 0 && array[i] < (array[i - 1] * array[i - 1]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = {5, 9, 10};
+
+            if (Quadrato(array) == true)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+                
         }
     }
 }
